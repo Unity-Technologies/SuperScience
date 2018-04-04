852a3269fa4f92137aeb2017bc7ea63ac6e5a80c
Andrew Maneri <andrewm@unity3d.com>
2018-04-03 23:41:13 -0400

    WIP on the physicsTracker

--- PhysicsTracker/PhysicsTracker.cs
+++ PhysicsTracker/PhysicsTracker.cs
@@ -23,27 +23,28 @@ namespace Unity.Labs.SuperScience
         /// <summary>
         /// The time period stored within a simple step sample
         /// </summary>
-        const float k_ChunkPeriod = k_Period/k_Steps;
+        const float k_SamplePeriod = k_Period/k_Steps;
 
         /// <summary>
         /// Weight to use for the most recent physics sample, when doing prediction
         /// </summary>
-        const float k_NewChunkWeight = 2.0f;
+        const float k_NewSampleWeight = 2.0f;
         
         /// <summary>
         /// Stores one sample of physics data
-        /// Time is used to measure how much more frame samples should be accumulated here
         /// </summary>
-        struct Chunk
+        struct Sample
         {
-            float time;
-            float distance;
-            float angle;
+            public float distance;
+            public float angle;
         }
 
-        int m_ChunkIndex = 0;
-        Chunk[] m_Chunks = new Chunk[k_Steps];
-
+        // We store the time elapsed in the current sample, so we can determine
+        // when enough data has been accumulated, and how much to fade out the 
+        // last sample in the list for smooth transitions
+        float m_SampleTime = 0.0f;
+        Sample[] m_Samples = new Sample[k_Steps + 1];
+        
         // Previous-frame history for integrating velocity
         Vector3 m_LastPosition = Vector3.zero;
         Quaternion m_LastRotation = Quaternion.identity;
@@ -56,121 +57,126 @@ namespace Unity.Labs.SuperScience
         public float AngularSpeed { get; set; }
         public Vector3 AngularAxis { get; set; }
         public Vector3 AngularVelocity { get; set; }
-        
-        // public Vector3 currentVelocity = current direction * speed
-
-        // Angular Motion
-        //public Vector3 currentAngularVelocity = rotationAxis * currentSpeed * deg2rad
-        
+ 
         public void Reset(Vector3 currentPosition, Quaternion currentRotation, 
                             Vector3 currentVelocity, Vector3 currentAngularVelocity)
         {
+            // Reset history values
+            m_LastPosition = currentPosition;
+            m_LastRotation = currentRotation;
+
+            // Then get new 'current' values based on this new history
+            Speed = currentVelocity.magnitude;
+            Direction = currentVelocity.normalized;
             Velocity = currentVelocity;
+
+            AngularSpeed = currentAngularVelocity.magnitude * Mathf.Rad2Deg;
+            AngularAxis = currentAngularVelocity.normalized;
             AngularVelocity = currentAngularVelocity;
-            m_ChunkIndex = 0;
-        }
-        // Initializes this tracker to a safe starting point    
-        public void Initialize(Vector3 startPoint, Quaternion startRotation)
-        {
-            // Since this is not a monobehavior we don't have OnValidate - which means we need to do input validation here
-            period = Mathf.Max(period, .01f);
-            steps = Mathf.Max(steps, 1);
+ 
+            var constantDistance = Speed * k_SamplePeriod;
+            var constantAngle = AngularSpeed * k_SamplePeriod;
 
-            m_ChunkPeriod = period / steps;
+            // Reset the chunks to match this history as well
+            for (var i = 0; i < k_Steps; i++)
+            {
+                m_Samples[i] = new Sample { angle = constantAngle, distance = constantDistance };
+            }
 
-            m_DistanceChunks = new float[steps];
-            m_AngleChunks = new float[steps];
-            m_TimeChunks = new float[steps];
-
-            ForceChangePosition(startPoint);
-            ForceChangeRotation(startRotation);
-
-            m_Initialized = true;
+            // The last sample is reset to 0, as it will pull in new values from our input sources
+            m_SampleTime = 0.0f;
+            m_Samples[k_Steps] = new Sample { angle = 0, distance = 0 };
         }
 
         public void Update(Vector3 newPosition, Quaternion newRotation, float timeSlice)
         {
-            if (!m_Initialized)
-                Initialize(newPosition, newRotation);
-
-            // Update the stored time and distance values
-            if (m_TimeChunks[m_ChunkIndex] > m_ChunkPeriod)
+            if (timeSlice <= 0.0f)
             {
-                m_ChunkIndex = (m_ChunkIndex + 1) % steps;
-                m_DistanceChunks[m_ChunkIndex] = 0;
-                m_AngleChunks[m_ChunkIndex] = 0;
-                m_TimeChunks[m_ChunkIndex] = 0;
+                return;
             }
-            m_TimeChunks[m_ChunkIndex] += timeSlice;
 
-            // Update positions and distance value
+            // Update linear data
             var currentOffset = newPosition - m_LastPosition;
-            m_CurrentDirection = currentOffset.normalized;
-            var newDistance = currentOffset.magnitude;
-            m_LastPosition = newPosition;
-            m_DistanceChunks[m_ChunkIndex] += newDistance;
 
-            // Update angles and rotation value
+            // Smoothed direction does not feel correct, so we calculate it from the most recent delta value
+            Direction = currentOffset.normalized;
+            var currentDistance = currentOffset.magnitude;
+            m_LastPosition = newPosition;
+
+            // Update angular data
             var rotationOffset = newRotation * Quaternion.Inverse(m_LastRotation);
             float currentAngle;
-            rotationOffset.ToAngleAxis(out currentAngle, out m_CurrentRotationAxis);
+            var tempAxis = Vector3.zero;
+            rotationOffset.ToAngleAxis(out currentAngle, out tempAxis);
+            AngularAxis = tempAxis;
             m_LastRotation = newRotation;
-            m_AngleChunks[m_ChunkIndex] += currentAngle;
-
-            // Update the average velocity
-            var totalDistance = 0.0f;
-            var totalRotation = 0.0f;
-            var totalTime = 0.0f;
-            var chunkCounter = 0;
-            while (chunkCounter < steps)
+        
+            // If the time slice passed in is bigger than the amount we hold in our buffers,
+            // we don't bother trying to smooth and do a reset instead
+            if (timeSlice > k_Period)
             {
-                if (chunkCounter == m_ChunkIndex)
-                {
-                    totalDistance += m_DistanceChunks[chunkCounter] * newChunkWeight;
-                    totalRotation += m_AngleChunks[chunkCounter] * newChunkWeight;
-                    totalTime += m_TimeChunks[chunkCounter] * newChunkWeight;
-                }
-                else
-                {
-                    totalDistance += m_DistanceChunks[chunkCounter];
-                    totalRotation += m_AngleChunks[chunkCounter];
-                    totalTime += m_TimeChunks[chunkCounter];
-                }
-                chunkCounter++;
+                var fullVelocityTime = 1.0f / timeSlice;
+                var newVelocity = currentOffset * fullVelocityTime;
+                var angularVelocity = AngularAxis * (currentAngle * fullVelocityTime);
+                Reset(newPosition, newRotation, newVelocity, angularVelocity);
+                return;
             }
 
-            if (totalTime > 0.0f)
+            // Attempt to fill up the most recent sample
+            var timeToAdd = Mathf.Min(timeSlice, k_SamplePeriod - m_SampleTime);
+            var timePercent = (timeToAdd / timeSlice);
+            timeSlice -= timeToAdd;
+            m_SampleTime += timeToAdd;
+
+            m_Samples[k_Steps].distance += currentDistance * timePercent;
+            m_Samples[k_Steps].angle += currentAngle * timePercent;
+
+            // Did we fill up our sample?
+            if (timeSlice > 0)
             {
-                currentSpeed = totalDistance / totalTime;
-                m_CurrentRotationSpeed = totalRotation / totalTime;
+                var sampleShift = Mathf.FloorToInt(timeSlice / k_SamplePeriod) + 1;
+                var shiftEnd = k_Steps - sampleShift + 1;
+
+                // Shift the remaining data as much as needed
+                for (var i = 0; i < shiftEnd; i++)
+                {
+                    m_Samples[i] = m_Samples[i + sampleShift];
+                }
+
+                // Any data over 1 sample shift gets loaded in with a full sample of data
+                for (var i = shiftEnd; i < k_Steps; i++)
+                {
+                    m_Samples[i].distance = currentDistance * k_SamplePeriod;
+                    m_Samples[i].angle = currentAngle * k_SamplePeriod;
+                    timeSlice -= m_SampleTime;
+                }
+
+                // Fill in the final 'current' sample of data
+                m_Samples[k_Steps].distance = currentDistance * timeSlice;
+                m_Samples[k_Steps].angle = currentAngle * timeSlice;
+                m_SampleTime = timeSlice;
             }
-        }
 
-        /// <summary>
-        /// Clear the buffer and set a new position
-        /// </summary>
-        /// <param name="newPosition">The new position</param>
-        public void ForceChangePosition(Vector3 newPosition)
-        {
-            m_LastPosition = newPosition;
-            ClearChunks();
-        }
+            // Generate the average sample
+            var edgeBlend = (m_SampleTime / k_SamplePeriod);
+            var invEdgeBlend = 1.0f - edgeBlend;
+            var totalDistance = m_Samples[0].distance * invEdgeBlend;
+            var totalAngle = m_Samples[0].angle * invEdgeBlend;
+ 
+            for (var i = 1; i < k_Steps; i++)
+            {
+                totalDistance += m_Samples[i].distance;
+                totalAngle += m_Samples[i].distance;
+            }
 
-        /// <summary>
-        /// Clear the buffer and set a new rotation
-        /// </summary>
-        /// <param name="newRotation">The new rotation</param>
-        public void ForceChangeRotation(Quaternion newRotation)
-        {
-            m_LastRotation = newRotation;
-            ClearChunks();
-        }
+            totalDistance += m_Samples[k_Steps].distance * edgeBlend * k_NewSampleWeight;
+            totalAngle += m_Samples[k_Steps].angle * edgeBlend * k_NewSampleWeight;
 
-        void ClearChunks()
-        {
-            Array.Clear(m_DistanceChunks, 0, m_DistanceChunks.Length);
-            Array.Clear(m_AngleChunks, 0, m_AngleChunks.Length);
-            Array.Clear(m_TimeChunks, 0, m_TimeChunks.Length);
+            Speed = totalDistance / k_Period;
+            Velocity = Direction * Speed;
+
+            AngularSpeed = totalAngle / k_Period;
+            AngularVelocity = AngularAxis * AngularSpeed * Mathf.Deg2Rad;
         }
     }
 }
