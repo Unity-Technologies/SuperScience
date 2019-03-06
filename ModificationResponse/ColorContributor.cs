39c3e088fc5ef212605556d2c595869f2b76ae85
Amy DiGiovanni <amyd@unity3d.com>
2019-03-06 10:37:30 -0800

    Added example of delayed response to property modification

---
+++ ModificationResponse/ColorContributor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Unity.Labs.SuperScience
+{
+    public class ColorContributor : MonoBehaviour
+    {
+        [SerializeField]
+        Color m_Color;
+
+        public Color color { get { return m_Color; } }
+    }
+}
---
+++ ModificationResponse/Editor/ModificationResponseExampleWindow.cs
@@ -0,0 +1,80 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Unity.Labs.SuperScience
+{
+    public class ModificationResponseExampleWindow : EditorWindow
+    {
+        const double k_TimeToResponse = 0.3;
+
+        double m_TimeOfLastChange = -1;
+        Color m_AverageColor;
+
+        [MenuItem("Window/Modification Response Example")]
+        static void Init()
+        {
+            var window = GetWindow<ModificationResponseExampleWindow>("Modification Response Example");
+            window.Show();
+        }
+
+        void OnEnable()
+        {
+            UpdateAverageColor();
+            Undo.postprocessModifications += OnPostprocessModifications;
+            Undo.undoRedoPerformed += OnUndoRedoPerformed;
+        }
+
+        void OnDisable()
+        {
+            Undo.postprocessModifications -= OnPostprocessModifications;
+            Undo.undoRedoPerformed -= OnUndoRedoPerformed;
+        }
+
+        void OnGUI()
+        {
+            EditorGUI.DrawRect(new Rect(Vector2.zero, position.size), m_AverageColor);
+        }
+
+        void Update()
+        {
+            if (m_TimeOfLastChange >= 0 && EditorApplication.timeSinceStartup - m_TimeOfLastChange >= k_TimeToResponse)
+            {
+                m_TimeOfLastChange = -1;
+                UpdateAverageColor();
+            }
+        }
+
+        UndoPropertyModification[] OnPostprocessModifications(UndoPropertyModification[] modifications)
+        {
+            foreach (var modification in modifications)
+            {
+                if (modification.currentValue.target is ColorContributor)
+                {
+                    m_TimeOfLastChange = EditorApplication.timeSinceStartup;
+                    break;
+                }
+            }
+
+            return modifications;
+        }
+
+        void OnUndoRedoPerformed()
+        {
+            if (Selection.activeGameObject.GetComponent<ColorContributor>())
+                m_TimeOfLastChange = EditorApplication.timeSinceStartup;
+        }
+
+        void UpdateAverageColor()
+        {
+            m_AverageColor = Color.clear;
+            var colorContributors = FindObjectsOfType<ColorContributor>();
+            foreach (var colorContributor in colorContributors)
+            {
+                m_AverageColor += colorContributor.color;
+            }
+
+            m_AverageColor /= colorContributors.Length;
+            Repaint();
+        }
+    }
+}
