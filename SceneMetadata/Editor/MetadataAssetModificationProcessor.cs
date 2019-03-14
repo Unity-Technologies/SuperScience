34059430c2b5bc3c34b4d9aa1f66b409749d0374
Amy DiGiovanni <amyd@unity3d.com>
2019-03-14 13:57:35 -0700

    Changed metadata example to use asset labels, and added other sample scenes

--- SceneMetadata/Editor/MetadataAssetModificationProcessor.cs
+++ SceneMetadata/Editor/MetadataAssetModificationProcessor.cs
@@ -1,4 +1,6 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,38 +8,51 @@ namespace Unity.Labs.SuperScience
 {
     class MetadataAssetModificationProcessor : UnityEditor.AssetModificationProcessor
     {
-        const string k_ExampleScenePath = "SuperScience/SceneMetadata/ExampleScene.unity";
+        const string k_MetadataExampleLabel = "MetadataExample";
         const string k_MetadataSuffix = "Metadata.asset";
+
+        static readonly List<string> k_MetadataPaths = new List<string>();
 
         static string[] OnWillSaveAssets(string[] paths)
         {
+            k_MetadataPaths.Clear();
             foreach (var path in paths)
             {
-                if (path.EndsWith(k_ExampleScenePath))
+                var scene = SceneManager.GetSceneByPath(path);
+                if (scene.IsValid())
                 {
-                    var metadataPath = path.Substring(0, path.Length - 6) + k_MetadataSuffix;
-                    var metadata = AssetDatabase.LoadAssetAtPath<ExampleSceneMetadata>(metadataPath);
-                    if (metadata == null)
+                    // We check Asset labels to contain metadata management to just Scenes involved in this example,
+                    // but depending on your use case you may have a different qualifying check or even none at all
+                    // if you want every Scene to have metadata.
+                    var sceneLabels = AssetDatabase.GetLabels(AssetDatabase.LoadAssetAtPath<SceneAsset>(path));
+                    if (sceneLabels.Contains(k_MetadataExampleLabel))
                     {
-                        // Create metadata if it doesn't exist
-                        metadata = ScriptableObject.CreateInstance<ExampleSceneMetadata>();
-                        AssetDatabase.CreateAsset(metadata, metadataPath);
+                        var metadataPath = path.Substring(0, path.Length - 6) + k_MetadataSuffix;
+                        var metadata = AssetDatabase.LoadAssetAtPath<ExampleSceneMetadata>(metadataPath);
+                        if (metadata == null)
+                        {
+                            // Create metadata if it doesn't exist
+                            metadata = ScriptableObject.CreateInstance<ExampleSceneMetadata>();
+                            AssetDatabase.CreateAsset(metadata, metadataPath);
+                        }
+
+                        metadata.UpdateFromScene(scene);
+
+                        // To make sure the metadata Asset gets saved, we need to both dirty the Asset and
+                        // include its path in the string array returned from OnWillSaveAssets.
+                        EditorUtility.SetDirty(metadata);
+                        k_MetadataPaths.Add(metadataPath);
                     }
-
-                    var scene = SceneManager.GetSceneByPath(path);
-                    metadata.UpdateFromScene(scene);
-
-                    // To make sure the metadata Asset gets saved, we need to both dirty the Asset and
-                    // include its path in the string array returned from OnWillSaveAssets.
-                    EditorUtility.SetDirty(metadata);
-                    var pathsToSave = new string[paths.Length + 1];
-                    paths.CopyTo(pathsToSave, 0);
-                    pathsToSave[pathsToSave.Length - 1] = metadataPath;
-                    return pathsToSave;
                 }
             }
 
-            return paths;
+            if (k_MetadataPaths.Count == 0)
+                return paths;
+
+            var pathsToSave = new string[paths.Length + k_MetadataPaths.Count];
+            paths.CopyTo(pathsToSave, 0);
+            k_MetadataPaths.CopyTo(pathsToSave, paths.Length);
+            return pathsToSave;
         }
     }
 }
