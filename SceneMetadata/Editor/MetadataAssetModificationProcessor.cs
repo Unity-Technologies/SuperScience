764bdae1a0fd644fef12735d8b0a4dd3839b13d9
Amy DiGiovanni <amyd@unity3d.com>
2019-03-14 11:12:37 -0700

    Changed assets in sync example to be specifically about scene metadata

---
+++ SceneMetadata/Editor/MetadataAssetModificationProcessor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Unity.Labs.SuperScience
+{
+    class MetadataAssetModificationProcessor : UnityEditor.AssetModificationProcessor
+    {
+        const string k_ExampleScenePath = "SuperScience/SceneMetadata/ExampleScene.unity";
+        const string k_MetadataSuffix = "Metadata.asset";
+
+        static string[] OnWillSaveAssets(string[] paths)
+        {
+            foreach (var path in paths)
+            {
+                if (path.EndsWith(k_ExampleScenePath))
+                {
+                    var metadataPath = path.Substring(0, path.Length - 6) + k_MetadataSuffix;
+                    var metadata = AssetDatabase.LoadAssetAtPath<ExampleSceneMetadata>(metadataPath);
+                    if (metadata == null)
+                    {
+                        // Create metadata if it doesn't exist
+                        metadata = ScriptableObject.CreateInstance<ExampleSceneMetadata>();
+                        AssetDatabase.CreateAsset(metadata, metadataPath);
+                    }
+
+                    var scene = SceneManager.GetSceneByPath(path);
+                    metadata.UpdateFromScene(scene);
+
+                    // To make sure the metadata Asset gets saved, we need to both dirty the Asset and
+                    // include its path in the string array returned from OnWillSaveAssets.
+                    EditorUtility.SetDirty(metadata);
+                    var pathsToSave = new string[paths.Length + 1];
+                    paths.CopyTo(pathsToSave, 0);
+                    pathsToSave[pathsToSave.Length - 1] = metadataPath;
+                    return pathsToSave;
+                }
+            }
+
+            return paths;
+        }
+    }
+}
---
+++ SceneMetadata/ExampleSceneMetadata.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Unity.Labs.SuperScience
+{
+    public class ExampleSceneMetadata : ScriptableObject
+    {
+        [SerializeField]
+        int m_TotalComponents;
+
+        static readonly List<GameObject> k_RootGameObjects = new List<GameObject>();
+        static readonly List<Component> k_Components = new List<Component>();
+
+        public void UpdateFromScene(Scene scene)
+        {
+            m_TotalComponents = 0;
+            scene.GetRootGameObjects(k_RootGameObjects);
+            foreach (var gameObject in k_RootGameObjects)
+            {
+                gameObject.GetComponentsInChildren(k_Components);
+                m_TotalComponents += k_Components.Count;
+            }
+        }
+    }
+}
