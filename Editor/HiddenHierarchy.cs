7d766d55a949ac572fe61d1ad2f3f60e7ff95a6d
Matt Schoen <schoen@unity3d.com>
2020-06-28 04:51:36 -0700

    Tweak GUI styles;
    Add preview scene;
    Add SetExpandedRecursively to recursively expand/collapse with alt-click;
    Code style cleanup

--- Editor/HiddenHierarchy.cs
+++ Editor/HiddenHierarchy.cs
@@ -11,51 +11,82 @@ namespace Unity.Labs.SuperScience
         static class Styles
         {
             const float k_RowHeight = 16f;
+            public static readonly GUIStyle SelectedRow;
+            public static readonly GUIStyle UnSelectedRow;
             public static readonly GUIStyle SceneFoldout;
-            public static readonly GUIStyle ClickableFoldout;
-            public static readonly GUIStyle ClickableFoldoutButton;
+            public static readonly GUIStyle FoldoutButton;
+            public static readonly GUIStyle FoldoutButtonExpanded;
             public static readonly GUIStyle ClickableLabel;
 
             static Styles()
             {
+                SelectedRow = new GUIStyle
+                {
+                    normal = new GUIStyleState { background = Texture2D.grayTexture },
+                    fixedHeight = k_RowHeight
+                };
+
+                UnSelectedRow = new GUIStyle
+                {
+                    hover = new GUIStyleState { background = Texture2D.grayTexture },
+                    fixedHeight = k_RowHeight
+                };
+
                 SceneFoldout = new GUIStyle(EditorStyles.foldout)
                 {
                     fontStyle = FontStyle.Bold
                 };
 
-                ClickableFoldoutButton = new GUIStyle(EditorStyles.label)
+                FoldoutButton = new GUIStyle
                 {
-                    padding = new RectOffset(-41, 1, 2, -2),
+                    normal = new GUIStyleState { background = EditorStyles.foldout.normal.scaledBackgrounds[0]},
+
+                    // Margin and overflow found by trial and error
+                    margin = new RectOffset(0,0, 6, 0),
+                    overflow = new RectOffset(3, 0, 4, -7),
+                    stretchWidth = false,
+                    fixedWidth = k_FoldoutButtonWidth,
                     fixedHeight = k_RowHeight
                 };
 
-                ClickableFoldout = new GUIStyle(EditorStyles.foldout)
+                FoldoutButtonExpanded = new GUIStyle(FoldoutButton)
                 {
-                    stretchWidth = false,
-                    fixedHeight = k_RowHeight
+                    normal = new GUIStyleState { background = EditorStyles.foldout.onNormal.scaledBackgrounds[0]},
                 };
 
                 ClickableLabel = new GUIStyle(EditorStyles.label)
                 {
-                    fixedHeight = k_RowHeight
+                    fixedHeight = k_RowHeight,
+                    margin = new RectOffset(0, 0, 0, 0),
+                    padding = new RectOffset(0, 0, 0, 0)
                 };
             }
         }
 
         const float k_Indent = 15f;
-        const float k_FoldoutArrowSize = 12f;
+        const float k_FoldoutButtonWidth = 10f;
         const string k_FreeGameobjectsLabel = "Free GameObjects";
+        const string k_RefreshButtonLabel = "Refresh";
+        const string k_AutoUpdateFieldLabel = "Auto Update";
+        const string k_PreviewSceneLabel = "Preview Scene";
+        const string k_UntitledSceneName = "Untitled";
+        const int k_RefreshButtonWidth = 50;
 
-        // Fields of SerializedObjects are serialized even if not marked with [SerializedField]
+        // Serializable fields of SerializedObjects survive domain reload even if not marked with [SerializedField]
         Vector2 m_ScrollPosition;
         bool m_AutoUpdate = true;
         List<bool> m_SceneFoldoutStates = new List<bool>();
         bool m_FreeGameObjectsFoldout = true;
 
+        // Dictionary cannot not be serialized, so foldout states will not survive domain reload
+        readonly Dictionary<GameObject, bool> m_GameObjectExpandedStates = new Dictionary<GameObject, bool>();
+
         // Non-serialized state stored here for non-auto-refresh mode
         readonly List<KeyValuePair<string, List<GameObject>>> m_RootObjectLists = new List<KeyValuePair<string, List<GameObject>>>();
         readonly List<GameObject> m_FreeGameObjects = new List<GameObject>();
-        readonly Dictionary<GameObject, bool> m_GameObjectFoldoutStates = new Dictionary<GameObject, bool>();
+
+        // Local method use only -- created here to reduce garbage collection. Collections must be cleared before use
+        static readonly Dictionary<string, List<GameObject>> k_GameObjectScenePathMap = new Dictionary<string, List<GameObject>>();
 
         [MenuItem("Window/SuperScience/HiddenHierarchy")]
         static void OnMenuItem()
@@ -77,10 +108,11 @@ namespace Unity.Labs.SuperScience
             var sceneCount = SceneManager.sceneCount;
 
             // Set up a dictionary to map scene path -> gameobject for faster lookup below
-            var gameObjectScenePathMap = new Dictionary<string, List<GameObject>>(sceneCount);
+            k_GameObjectScenePathMap.Clear();
             m_FreeGameObjects.Clear();
             foreach (var gameObject in gameObjects)
             {
+                // We only care about root objects
                 if (gameObject.transform.parent != null)
                     continue;
 
@@ -95,13 +127,19 @@ namespace Unity.Labs.SuperScience
                 }
 
                 var scenePath = scene.path;
-                if (!gameObjectScenePathMap.TryGetValue(scenePath, out var list))
+                if (!k_GameObjectScenePathMap.TryGetValue(scenePath, out var list))
                 {
                     list = new List<GameObject>();
-                    gameObjectScenePathMap[scenePath] = list;
+                    k_GameObjectScenePathMap[scenePath] = list;
                 }
 
                 list.Add(gameObject);
+            }
+
+            // Sort by sibling index so that order matches actual hierarchy
+            foreach (var kvp in k_GameObjectScenePathMap)
+            {
+                kvp.Value.Sort((a, b) => a.transform.GetSiblingIndex().CompareTo(b.transform.GetSiblingIndex()));
             }
 
             // Store the objects in a list of lists to keep scene sorting order
@@ -112,30 +150,35 @@ namespace Unity.Labs.SuperScience
                 if (!scene.IsValid())
                     continue;
 
-                if (!gameObjectScenePathMap.TryGetValue(scene.path, out var list))
+                if (!k_GameObjectScenePathMap.TryGetValue(scene.path, out var list))
                     continue;
 
-                list.Sort((a, b) => a.transform.GetSiblingIndex().CompareTo(b.transform.GetSiblingIndex()));
                 var sceneName = scene.name;
+
+                // Untitled scene will have a blank scene name; replace with Untitled to match actual hierarchy
                 if (string.IsNullOrEmpty(sceneName))
-                    sceneName = "Untitled";
+                    sceneName = k_UntitledSceneName;
 
                 m_RootObjectLists.Add(new KeyValuePair<string, List<GameObject>>(sceneName, list));
             }
+
+            // Objects in a valid scene with no path are in the Preview Scene
+            if (k_GameObjectScenePathMap.TryGetValue(string.Empty, out var previewObjects))
+                m_RootObjectLists.Add(new KeyValuePair<string, List<GameObject>>(k_PreviewSceneLabel, previewObjects));
         }
 
         void OnGUI()
         {
             using (new GUILayout.HorizontalScope())
             {
-                m_AutoUpdate = EditorGUILayout.Toggle("Auto Update", m_AutoUpdate);
+                m_AutoUpdate = EditorGUILayout.Toggle(k_AutoUpdateFieldLabel, m_AutoUpdate);
                 if (m_AutoUpdate)
                     Refresh();
 
-                using (new GUILayout.HorizontalScope(GUILayout.Width(50)))
+                using (new GUILayout.HorizontalScope(GUILayout.Width(k_RefreshButtonWidth)))
                 using (new EditorGUI.DisabledScope(m_AutoUpdate))
                 {
-                    if (GUILayout.Button("Refresh"))
+                    if (GUILayout.Button(k_RefreshButtonLabel))
                         Refresh();
                 }
             }
@@ -179,43 +222,63 @@ namespace Unity.Labs.SuperScience
             }
         }
 
-        void DrawObject(GameObject go, float indent)
+        void DrawObject(GameObject gameObject, float indent)
         {
-            if (go == null)
+            if (gameObject == null)
                 return;
 
-            var transform = go.transform;
+            var transform = gameObject.transform;
 
-            var foldout = false;
-            using (new GUILayout.HorizontalScope())
+            var expanded = false;
+            var rowStyle = Selection.activeGameObject == gameObject ? Styles.SelectedRow : Styles.UnSelectedRow;
+            using (new GUILayout.VerticalScope(rowStyle))
             {
-                GUILayout.Space(indent);
-                GUIStyle labelStyle;
-                if (transform.childCount > 0)
+                using (new GUILayout.HorizontalScope())
                 {
-                    m_GameObjectFoldoutStates.TryGetValue(go, out foldout);
-                    foldout = EditorGUILayout.Foldout(foldout, string.Empty, false, Styles.ClickableFoldout);
-                    m_GameObjectFoldoutStates[go] = foldout;
+                    GUILayout.Space(indent);
+                    if (transform.childCount > 0)
+                    {
+                        m_GameObjectExpandedStates.TryGetValue(gameObject, out expanded);
+                        var style = expanded ? Styles.FoldoutButtonExpanded : Styles.FoldoutButton;
 
-                    labelStyle = Styles.ClickableFoldoutButton;
-                }
-                else
-                {
-                    GUILayout.Space(k_FoldoutArrowSize);
-                    labelStyle = Styles.ClickableLabel;
-                }
+                        // Foldouts without labels take up some minimum width and steal clicks from the label button
+                        // We emulate a foldout with a button and custom styles so that we can use a button to display
+                        // the gameobject label and set selection on click
+                        if (GUILayout.Button(string.Empty, style))
+                        {
+                            expanded = !expanded;
+                            if (Event.current.alt)
+                                SetExpandedRecursively(gameObject, expanded);
+                        }
 
-                var content = new GUIContent(go.name, AssetPreview.GetMiniThumbnail(go));
-                if (GUILayout.Button(content, labelStyle))
-                    Selection.activeObject = go;
+                        m_GameObjectExpandedStates[gameObject] = expanded;
+                    }
+                    else
+                    {
+                        GUILayout.Space(k_FoldoutButtonWidth);
+                    }
+
+                    var content = new GUIContent(gameObject.name, AssetPreview.GetMiniThumbnail(gameObject));
+                    if (GUILayout.Button(content, Styles.ClickableLabel))
+                        Selection.activeObject = gameObject;
+                }
             }
 
-            if (!foldout)
+            if (!expanded)
                 return;
 
             foreach (Transform child in transform)
             {
                 DrawObject(child.gameObject, indent + k_Indent);
+            }
+        }
+
+        void SetExpandedRecursively(GameObject gameObject, bool expanded)
+        {
+            m_GameObjectExpandedStates[gameObject] = expanded;
+            foreach (Transform child in gameObject.transform)
+            {
+                SetExpandedRecursively(child.gameObject, expanded);
             }
         }
     }
