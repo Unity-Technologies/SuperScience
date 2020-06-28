4f89f8dcfbe99c8e98fda84c4ce600b69155abe9
Matt Schoen <schoen@unity3d.com>
2020-06-28 01:29:22 -0700

    Update HiddenHierarchy to show hidden root objects and "free objects" outside of the scene

--- Editor/HiddenHierarchy.cs
+++ Editor/HiddenHierarchy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -12,6 +13,9 @@ namespace Unity.Labs.SuperScience
         static readonly Dictionary<GameObject, bool> k_FoldoutStates = new Dictionary<GameObject, bool>();
 
         Vector2 m_ScrollPosition;
+        bool m_AutoUpdate = true;
+        readonly List<KeyValuePair<string, List<GameObject>>> m_RootObjectLists = new List<KeyValuePair<string, List<GameObject>>>();
+        readonly List<GameObject> m_FreeGameObjects = new List<GameObject>();
 
         [MenuItem("Window/SuperScience/HiddenHierarchy")]
         static void OnMenuItem()
@@ -21,39 +25,109 @@ namespace Unity.Labs.SuperScience
 
         void OnEnable()
         {
-            Selection.selectionChanged += Repaint;
+            autoRepaintOnSceneChange = m_AutoUpdate;
+            if (!m_AutoUpdate)
+                Refresh();
         }
 
-        void OnDisable()
+        void Refresh()
         {
-            Selection.selectionChanged -= Repaint;
+            // Resources.FindObjectsOfTypeAll will find hidden root objects that scene.GetRootGameObjects doesn't contain
+            var gameObjects = Resources.FindObjectsOfTypeAll<GameObject>();
+            var sceneCount = SceneManager.sceneCount;
+
+            // Set up a dictionary to map scene path -> gameobject for faster lookup below
+            var gameObjectScenePathMap = new Dictionary<string, List<GameObject>>(sceneCount);
+            m_FreeGameObjects.Clear();
+            foreach (var gameObject in gameObjects)
+            {
+                if (gameObject.transform.parent != null)
+                    continue;
+
+                var scene = gameObject.scene;
+                if (!scene.IsValid())
+                {
+                    // If the scene is not valid, and this isn't a prefab, it is a "free floating" GameObject
+                    if (!PrefabUtility.IsPartOfPrefabAsset(gameObject))
+                        m_FreeGameObjects.Add(gameObject);
+
+                    continue;
+                }
+
+                var scenePath = scene.path;
+                if (!gameObjectScenePathMap.TryGetValue(scenePath, out var list))
+                {
+                    list = new List<GameObject>();
+                    gameObjectScenePathMap[scenePath] = list;
+                }
+
+                list.Add(gameObject);
+            }
+
+            // Store the objects in a list of lists to keep scene sorting order
+            m_RootObjectLists.Clear();
+            for (var i = 0; i < sceneCount; i++)
+            {
+                var scene = SceneManager.GetSceneAt(i);
+                if (!scene.IsValid())
+                    continue;
+
+                if (!gameObjectScenePathMap.TryGetValue(scene.path, out var list))
+                    continue;
+
+                list.Sort((a, b) => a.transform.GetSiblingIndex().CompareTo(b.transform.GetSiblingIndex()));
+                var sceneName = scene.name;
+                if (string.IsNullOrEmpty(sceneName))
+                    sceneName = "Untitled";
+
+                m_RootObjectLists.Add(new KeyValuePair<string, List<GameObject>>(sceneName, list));
+            }
         }
 
         void OnGUI()
         {
+            using (new GUILayout.HorizontalScope())
+            {
+                m_AutoUpdate = EditorGUILayout.Toggle("Auto Update", m_AutoUpdate);
+                if (m_AutoUpdate)
+                    Refresh();
+
+                using (new GUILayout.HorizontalScope(GUILayout.Width(50)))
+                using (new EditorGUI.DisabledScope(m_AutoUpdate))
+                {
+                    if (GUILayout.Button("Refresh"))
+                        Refresh();
+                }
+            }
+
             using (var scrollView = new GUILayout.ScrollViewScope(m_ScrollPosition))
             {
                 m_ScrollPosition = scrollView.scrollPosition;
-                var sceneCount = SceneManager.sceneCount;
-                for (var i = 0; i < sceneCount; i++)
+                foreach (var kvp in m_RootObjectLists)
                 {
-                    var scene = SceneManager.GetSceneAt(i);
-                    if (!scene.IsValid())
-                        continue;
+                    var sceneName = kvp.Key;
+                    GUILayout.Label(sceneName, EditorStyles.boldLabel);
 
-                    GUILayout.Label(scene.name, EditorStyles.boldLabel);
-
-                    var rootObjects = scene.GetRootGameObjects();
+                    var rootObjects = kvp.Value;
                     foreach (var go in rootObjects)
                     {
                         DrawObject(go, k_Indent);
                     }
+                }
+
+                GUILayout.Label("Free GameObjects", EditorStyles.boldLabel);
+                foreach (var gameObject in m_FreeGameObjects)
+                {
+                    DrawObject(gameObject, k_Indent);
                 }
             }
         }
 
         static void DrawObject(GameObject go, float indent)
         {
+            if (go == null)
+                return;
+
             var transform = go.transform;
 
             var foldout = false;
